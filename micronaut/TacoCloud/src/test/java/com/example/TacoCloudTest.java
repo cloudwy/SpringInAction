e77c8d676504f043736127f25f2cdd015519c604
Yun Wu <yunwu@M08337-01.local>
2024-04-03 15:36:50 +0200

    INIT: migrate from Spring Boot to Micronaut

---
+++ micronaut/TacoCloud/src/test/java/com/example/TacoCloudTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+class TacoCloudTest {
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    public void testHello() {
+        HttpRequest<?> request = HttpRequest.GET("/hello").accept(MediaType.TEXT_PLAIN);
+        String body = client.toBlocking().retrieve(request);
+
+        assertNotNull(body);
+        assertEquals("Hello World", body);
+    }
+
+}
