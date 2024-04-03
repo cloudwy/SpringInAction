2c5d01f3b77dff532473692715a0f4642d52079c
Yun Wu <yunwu@M08337-01.local>
2024-04-03 21:53:41 +0200

    init ch2

---
+++ micronaut/ch2/TacoCloud/src/main/java/com/example/MessageService.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import io.micronaut.core.annotation.NonNull;
+import jakarta.validation.constraints.NotBlank;
+
+interface MessageService {
+    String sayHello(@NonNull @NotBlank String name);
+}
