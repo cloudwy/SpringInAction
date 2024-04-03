494dc1f9b6045cb27571845ce6eaf0ec0b8d0de8
Yun Wu <yunwu@M08337-01.local>
2024-04-03 09:09:38 +0200

    fix 3.2, init 3.3

---
+++ chapter3/jpa/src/main/java/com/example/TacoCloud/model/Ingredient.java
@@ -0,0 +1,31 @@
+package com.example.TacoCloud.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.lang.reflect.Type;
+
+@Data
+@Table
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class Ingredient implements Persistable<String> {
+    @Id
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    @Override
+    public boolean isNew() {
+        return true;
+    }
+
+    public enum Type{
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
