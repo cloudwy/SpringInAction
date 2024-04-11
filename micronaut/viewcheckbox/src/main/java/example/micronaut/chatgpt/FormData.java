589fda1f8bfa14f75d5d1364f8b642696a2cc7bf
Yun Wu <yunwu@M08337-01.local>
2024-04-11 16:16:56 +0200

    checkbox works

--- micronaut/viewcheckbox/src/main/java/example/micronaut/chatgpt/FormData.java
+++ micronaut/viewcheckbox/src/main/java/example/micronaut/chatgpt/FormData.java
@@ -9,8 +9,8 @@ import io.micronaut.serde.annotation.Serdeable;
 @ReflectiveAccess
 @Serdeable
 public class FormData {
-//    @JsonProperty
-//    boolean subscribeCheckBox;
+    @JsonProperty
+    boolean subscribeCheckBox;
     @JsonProperty
     String attributeInput;
 
@@ -18,20 +18,20 @@ public class FormData {
     }
 
     public FormData(
-//            boolean subscribeCheckBox,
+            boolean subscribeCheckBox,
             String attributeInput) {
-//        this.subscribeCheckBox = subscribeCheckBox;
+        this.subscribeCheckBox = subscribeCheckBox;
         this.attributeInput = attributeInput;
     }
 
     // Getter and setter for 'subscribe' property
-//    public boolean isSubscribeCheckBox() {
-//        return subscribeCheckBox;
-//    }
+    public boolean isSubscribeCheckBox() {
+        return subscribeCheckBox;
+    }
 
-//    public void setSubscribeCheckBox(boolean subscribeCheckBox) {
-//        this.subscribeCheckBox = subscribeCheckBox;
-//    }
+    public void setSubscribeCheckBox(boolean subscribeCheckBox) {
+        this.subscribeCheckBox = subscribeCheckBox;
+    }
 
     // Getter and setter for 'attribute' property
     public String getAttributeInput() {
@@ -42,14 +42,3 @@ public class FormData {
         this.attributeInput = attributeInput;
     }
 }
-
-//@ReflectiveAccess
-//@Serdeable
-//public record FormData(
-//        boolean subscribe,
-//        String attribute
-//){
-//
-//    public FormData {
-//    }
-//}
