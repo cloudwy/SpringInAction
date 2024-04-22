bca3a42ddfeb05cc425a66224b519334c154b327
Yun Wu <yunwu@M08337-01.local>
2024-04-22 17:18:37 +0200

    [Delete]useless files

--- micronaut/ch2/TacoCloud/src/main/java/com/example/web/MessageService.java
+++
@@ -1,8 +0,0 @@
-//package com.example.web;
-//
-//import io.micronaut.core.annotation.NonNull;
-//import jakarta.validation.constraints.NotBlank;
-//
-//public interface MessageService {
-//    String sayHello(@NonNull @NotBlank String name);
-//}
